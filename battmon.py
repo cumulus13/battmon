3700b101be39625bab8f4cad3dc12dcff30ebd3d
cumulus13 <cumulus13@gmail.com>
2022-11-14 15:20:45 +0700

    version: 0.15 ~ 2022-11-14 15:20:27:370605

--- battmon.py
+++ battmon.py
@@ -55,6 +55,7 @@ def set_color_plugged(status):
     return make_colors("===>", 'b', 'lg')
 
 def run():
+    nine = 0
     try:
         print(make_colors(datetime.strftime(datetime.now(), '%Y/%m/%d %H:%M:%S:%f'), 'lb') + " " + "-"*(cmdw.getWidth() - 30))
     except:
@@ -71,19 +72,24 @@ def run():
             )
             if int(battery.percent) == 100:
                 #try:
-                notify.send('Battery is FULL', 'Battery is FULL', 'battmon', 'FULL', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icon.png'))
+                notify.send('Battery is FULL', 'Battery is FULL', 'battmon', 'FULL', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), '100.png'))
                 #except:
                 print(make_colors("BATTERY FULL !", 'lw', 'r'))
                 time.sleep((config.get_config('sleep', 'fulltime', '1') or 1))
-            elif int(battery.percent) == 99 and battery.power_plugged:
+            elif int(battery.percent) == 99 and battery.power_plugged and not config.get_config('nine', 'done'): 
                 #try:
-                notify.send('Battery is 99', 'Battery is 99', 'battmon', 'FULL', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icon.png'))
+                notify.send('Battery is 99', 'Battery is 99', 'battmon', 'FULL', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), '90.png'))
                 #except:
-                print(make_colors("BATTERY FULL !", 'lw', 'r'))
-                time.sleep((config.get_config('sleep', 'willtime', '5') or 5))            
-            elif int(battery.percent) <= 10:
+                print(make_colors("BATTERY is 99 !", 'lw', 'r'))
+                time.sleep((config.get_config('sleep', 'willtime', '5') or 5))
+                if not nine == (config.get_config('nine', 'times', '5') or 5):
+                    nine += 1
+                else:
+                    config.write_config('nine', 'done', '1')
+            elif int(battery.percent) <= 10 and not battery.power_plugged:
+                config.write_config('nine', 'done', '0')
                 #try:
-                notify.send('Battery is LOW', 'Battery is LOW', 'battmon', 'LOW', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icon.png'))
+                notify.send('Battery is LOW', 'Battery is LOW', 'battmon', 'LOW', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), '10.png'))
                 #except:
                 print(make_colors("BATTERY LOW !", 'lw', 'r'))
                 time.sleep((config.get_config('sleep', 'lesstime', '5') or 5))    
@@ -92,8 +98,11 @@ def run():
             
             if int(battery.percent) in [11, 20, 30, 40, 50, 60, 70, 80, 90, 99]:
                 if config.get_config('remind', 'ten'):
-                    message = "Battery stat" + " : " + "[{}]".format(battery.percent) + "\n" + str(convertTime(battery.secsleft)) + "\n" + str(battery.power_plugged)
-                    notify.send('Battery STATUS', message, 'battmon', 'STATUS', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icon.png'))
+                    stat = "DC - Unplug"
+                    if battery.power_plugged:
+                        stat = "AC - Plug"
+                    message = "Battery stat" + " : " + "[{}]".format(battery.percent) + "\n" + str(convertTime(battery.secsleft)) + "\n" + stat
+                    notify.send('Battery STATUS', message, 'battmon', 'STATUS', icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), '{}.png'.format(battery.percent)))
             try:
                 print(make_colors(datetime.strftime(datetime.now(), '%Y/%m/%d %H:%M:%S:%f'), 'lb') + " " + "-"*(cmdw.getWidth() - 30))
             except:
